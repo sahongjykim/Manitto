7474f43cb271947e752d553399b632d2ad179046
200wonman <kjy_1615@naver.com>
2023-10-26 11:55:36 +0900

    dd

--- src/components/page/manito/ManitoSelect.vue
+++ src/components/page/manito/ManitoSelect.vue
@@ -11,7 +11,7 @@
   <button @click="registPlayer" :disabled="status">등록</button>
   <div>
     ------------
-    <button @click="drawManito" :disabled="status">마니또뽑기DDff</button>
+    <button @click="drawManito" :disabled="status">마니또뽑기DDffll</button>
     ------------
   </div>
   <h1>참여자 ({{ players.length > 0 ? players.length : "0" }})</h1>
