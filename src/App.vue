eed12b4b991b4b7f9177eba6ab97de3a8d31d144
200wonman <kjy_1615@naver.com>
2023-10-30 15:29:43 +0900

    퍼블리싱 셋팅중

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,6 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+@import "@/assets/scss/style.scss";
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,30 @@
-const { defineConfig } = require('@vue/cli-service')
+const path = require("path");
+const { defineConfig } = require("@vue/cli-service");
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  // productionSourceMap: process.env.NODE_ENV != 'production',
+  transpileDependencies: true,
+  // assets 단축 경로 설정
+  assetsDir: "src/assets/",
+  configureWebpack: {
+    resolve: {
+      alias: {
+        // scss 단축 경로 ( ./src/assets 를 ~styles로 사용 가능 )
+        styles: path.resolve("./src/assets"),
+      },
+    },
+    // plugins: [new BundleAnalyzerPlugin()],
+  },
+  lintOnSave: false,
+  // outputDir: path.resolve(__dirname, '../src/main/resources/static'),
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/assets/scss/style.scss";
+        `,
+      },
+    },
+  },
+});
